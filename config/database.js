1d2f4d67906166a229b6d5a71c45437d5e9f6d1b
muhamadazis32 <muhamadazis9a@gmail.com>
2022-06-09 18:55:18 +0700

    pg admin connect

--- config/database.js
+++ config/database.js
@@ -12,10 +12,11 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
-    port: DB_PORT,
+    // port: DB_PORT,
     dialect: "postgres",
+    ssl: true,
   },
   test: {
     username: DB_USER,
